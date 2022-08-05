d271c77b1235ecc5b4424847cea159e11c6084e0
Jimmy Chu <jimmychu0807@gmail.com>
2022-08-05 18:58:27 +0800

    Fixing script for CI (#19)
    * good on workspaces
    
    * updated
    
    * updated

--- scripts/docs-update.ts
+++ scripts/docs-update.ts
@@ -9,27 +9,28 @@ const DOCS_PATH = path.join(__dirname, '/../docs')
 const DOCS_README_PATH = path.join(DOCS_PATH, '/README.md')
 const DOCS_SIDEBAR_PATH = path.join(DOCS_PATH, '/_sidebar.md')
 
- // dirs are filtered to be the problem set in aoc 2021
+// dirs are filtered to be the problem set in aoc 2021
 const packageDirs = (): fs.Dirent[] =>
-  fs.readdirSync(PACKAGES_PATH, { encoding: 'utf-8', withFileTypes: true })
-    .filter(dir => dir.isDirectory() && dir.name.match(/^\d+\-.+$/))
+  fs
+    .readdirSync(PACKAGES_PATH, { encoding: 'utf-8', withFileTypes: true })
+    .filter((dir) => dir.isDirectory() && dir.name.match(/^\d+-.+$/))
 
 function genDocContent(contentPrefix: string, problemLinkFn: (name: string) => string): string {
   const dirs = packageDirs()
 
-  const problemList = dirs.map(dir => {
-    let folderNameArr = dir.name.split('-')
+  const problemList = dirs.map((dir) => {
+    const folderNameArr = dir.name.split('-')
     const dayNum = folderNameArr.shift()
-    return `- [Day ${dayNum} - ${capitalize(folderNameArr.join(' '))}](${problemLinkFn(dir.name)})\n`
+    return `- [Day ${dayNum} - ${capitalize(folderNameArr.join(' '))}](${problemLinkFn(dir.name)})`
   })
 
-  return contentPrefix.concat(...problemList)
+  return contentPrefix.replace('{{{REPLACEMENT}}}', problemList.join('\n'))
 }
 
 function copyPackagesReadmeOver(): void {
   const dirs = packageDirs()
 
-  dirs.forEach(dir => {
+  dirs.forEach((dir) => {
     const packageReadmePath = `${PACKAGES_PATH}/${dir.name}/README.md`
     const destPath = `${DOCS_PATH}/${dir.name}.md`
     fs.copyFileSync(packageReadmePath, destPath)
@@ -41,17 +42,17 @@ function copyPackagesReadmeOver(): void {
 const readmeTpl = fs.readFileSync(README_TPL_PATH, { encoding: 'utf-8' }) + '\n'
 
 console.log('1. Generate root README.md...')
-const rootReadmeContent = genDocContent(readmeTpl, dirName => `./packages/${dirName}`)
+const rootReadmeContent = genDocContent(readmeTpl, (dirName) => `./packages/${dirName}`)
 fs.writeFileSync(ROOT_README_PATH, rootReadmeContent)
 
 console.log('2. Generate docs/README.md...')
-const docsReadmeContent = genDocContent(readmeTpl, dirName => `/${dirName}.md`)
+const docsReadmeContent = genDocContent(readmeTpl, (dirName) => `/${dirName}.md`)
 fs.writeFileSync(DOCS_README_PATH, docsReadmeContent)
 
 console.log('3. Copy over packages README.md to docs...')
 copyPackagesReadmeOver()
 
 console.log('4. Generate docs/_sidebar.md...')
-const sbPrefix = '- [Home](/)\n'
-const sidebarContent = genDocContent(sbPrefix, dirName => `${dirName}.md`)
+const sbPrefix = '- [Home](/)\n{{{REPLACEMENT}}}'
+const sidebarContent = genDocContent(sbPrefix, (dirName) => `${dirName}.md`)
 fs.writeFileSync(DOCS_SIDEBAR_PATH, sidebarContent)
--- scripts/templates/README.md.tpl
+++ scripts/templates/README.md.tpl
@@ -3,3 +3,33 @@
 [![Known Vulnerabilities](https://snyk.io/test/github/jimmychu0807/advent-of-code-2021/badge.svg)](https://snyk.io/test/github/jimmychu0807/advent-of-code-2021) [![npm version](https://badge.fury.io/js/@aoc-2021%2Fcli.svg)](https://badge.fury.io/js/@aoc-2021%2Fcli)
 
 This is my attempt on [Advent of Code 2021](https://adventofcode.com/2021) :rocket:
+
+{{{REPLACEMENT}}}
+
+## Workflow
+
+**Normal run**
+
+```bash
+yarn install
+# More help text will be displayed
+yarn cli
+```
+
+Note when entering input file in relative path in `yarn cli`, the path is relative to `packages/cli` directory, not the current user directory.
+
+**For CI**
+
+```bash
+yarn install
+# More help text will be displayed
+yarn validate
+```
+
+**For docs generation**
+
+```bash
+yarn docs:update
+# Preview docs
+yarn docs:serve
+```
