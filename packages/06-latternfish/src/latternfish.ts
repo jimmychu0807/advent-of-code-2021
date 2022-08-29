9a7cc5a813342231f3079cec1fb9dec1386734ee
Jimmy Chu <jimmychu0807@gmail.com>
2022-08-29 23:51:47 +0800

    Completed Day 06 (#28)
    * finish problem 06a
    
    * [wip] on latternfish
    
    * added the unhandled example
    
    * Solving with Uint8Array
    
    * wip
    
    * able to load temporaryFile in
    
    * wip
    
    * much more efficient
    
    * update of 6b
    
    * End up to be simple, if you model the problem properly
    
    * Completed Day 06

---
+++ packages/06-latternfish/src/latternfish.ts
@@ -0,0 +1,44 @@
+'use struct'
+import Debug from 'debug'
+
+const log = {
+  info: Debug('latternfish')
+}
+
+interface LatternfishConfig {
+  fishes: Array<number>
+  dayToSpawn: number
+  initDayToSpawn: number
+  daySimulation: number
+}
+
+const summation = (arr: Array<number>) => arr.reduce((sum, v) => sum + v, 0)
+
+class Latternfish {
+  static modeling(config: LatternfishConfig): number {
+    const { fishes, dayToSpawn, initDayToSpawn, daySimulation } = config
+    const size = Math.max(dayToSpawn, initDayToSpawn) + 1
+    const insertion = Math.min(dayToSpawn, initDayToSpawn)
+
+    let result: Array<number> = fishes.reduce((memo, fish) => {
+      memo[fish] += 1
+      return memo
+    }, new Array(size).fill(0))
+
+    for (let i = 0; i < daySimulation; i++) {
+      const newSpawn = result[0] as number
+      result = result.slice(1, size)
+
+      // Add back the fish to give birth again
+      result[insertion] += newSpawn
+      // Newly spawned fish
+      result.push(newSpawn)
+
+      log.info(`End of day ${i + 1}: [${result}], summation: ${summation(result)}`)
+    }
+
+    return summation(result)
+  }
+}
+
+export { Latternfish as default, Latternfish, LatternfishConfig }
