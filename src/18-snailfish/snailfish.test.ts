d9d79f5f2195f9db916ebf0fef9a136ae3c53424
Jimmy Chu <jimmychu0807@gmail.com>
2023-01-10 22:57:38 +0800

    rewriting day18 in object

--- src/18-snailfish/snailfish.test.ts
+++ src/18-snailfish/snailfish.test.ts
@@ -2,32 +2,85 @@ import { expect } from "chai";
 
 import Snailfish from "./snailfish.js";
 
-const SAMPLE1 = {
+const PARSE_TO_TREE_SAMPLE = {
   input: "[[1,2],[[3,4],5]]",
-  // prettier-ignore
-  tree: {
-    left: { left: 1, right: 2 },
-    right: { left: { left: 3, right: 4 }, right: 5},
-  },
   magnitude: 143,
 };
 
+// const REDUCTION_SAMPLE = {
+//   input: "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]",
+//   reducedResult: "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]",
+// };
+
+// const SUM_SAMPLE1 = {
+//   input: [
+//     "[1,1]",
+//     "[2,2]",
+//     "[3,3]",
+//     "[4,4]",
+//     "[5,5]",
+//     "[6,6]",
+//   ],
+//   sumString: "[[[[5,0],[7,4]],[5,5]],[6,6]]",
+//   magnitude: 1137,
+// };
+
+// const SUM_SAMPLE2 = {
+//   input: [
+//     "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+//     "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+//     // "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+//     // "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+//     // "[7,[5,[[3,8],[1,4]]]]",
+//     // "[[2,[2,2]],[8,[8,1]]]",
+//     // "[2,9]",
+//     // "[1,[[[9,3],9],[[9,0],[0,7]]]]",
+//     // "[[[5,[7,4]],7],1]",
+//     // "[[[[4,2],2],6],[8,7]]",
+//   ],
+//   sumString: "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]",
+//   magnitude: 3488,
+// };
+
 describe("Day 18 - Snailfish", () => {
   describe("Part I", () => {
-    it("Snailfish.parseToTree() works", () => {
-      const { input, tree } = SAMPLE1;
-      const [resTree, cursor] = Snailfish.parseToTree(input);
-
-      console.log(resTree);
-
-      expect(cursor).to.eq(input.length);
-      expect(resTree).to.eql(tree);
+    it("Snailfish.parse() works", () => {
+      const { input } = PARSE_TO_TREE_SAMPLE;
+      const node = Snailfish.parse(input);
+      expect(node.toString()).to.eq(input);
     });
 
-    it("Snailfish.magnitude() works", () => {
-      const { input, magnitude } = SAMPLE1;
-      const res = Snailfish.parseToTree(input);
-      expect(Snailfish.magnitude(res[0])).to.eq(magnitude);
+    it("Node.magnitude() works", () => {
+      const { input, magnitude } = PARSE_TO_TREE_SAMPLE;
+      const node = Snailfish.parse(input);
+      expect(node.magnitude()).to.eq(magnitude);
     });
+
+    // it("Snailfish.reduceTree() works", () => {
+    //   const { input, reducedResult } = REDUCTION_SAMPLE;
+    //   const result = Snailfish.reduce(Snailfish.parse(input));
+    //   expect(result).to.eql(Snailfish.parse(reducedResult));
+    // });
+
+    // it("Snailfish.sum() and Snailfish.magnitide() works for SUM_SAMPLES", () => {
+    //   [SUM_SAMPLE2].forEach((sample, idx) => {
+    //     console.log(`SUM_SAMPLE${idx + 1} test.`);
+    //     const { input, sumString, magnitude } = sample;
+    //     const sumNode = input.slice(1).reduce((memo, ln) => {
+    //       const res = Snailfish.sum(memo, ln);
+
+    //       console.log(`final`);
+    //       console.dir(res, { depth: null });
+
+    //       return res;
+    //     }, Snailfish.parse(input[0]!));
+
+    //     expect(sumNode).to.eql(
+    //       Snailfish.parse(sumString),
+    //       `SUM_SAMPLE${idx + 1} don't match with expected`,
+    //     );
+    //     expect(Snailfish.magnitude(sumNode)).to.eq(magnitude);
+    //   });
+    // });
   });
 });
--- src/18-snailfish/snailfish.ts
+++ src/18-snailfish/snailfish.ts
@@ -1,12 +1,5 @@
 import { Direction } from "../utils/index.js";
 
-interface Node {
-  left: Node | number;
-  right: Node | number;
-}
-
-type ExplodePayload = [number, number] | undefined;
-
 const CharCode = {
   "0": "0".charCodeAt(0),
   "9": "9".charCodeAt(0),
@@ -15,34 +8,109 @@ const CharCode = {
 const SPLIT_THRESHOLD = 10;
 const EXPLODE_DEPTH = 4;
 
+class Node {
+  private branches: [Node, Node] | undefined;
+  private value: number | undefined;
+
+  constructor(param1: number | string | Node, param2?: Node) {
+    if ((typeof param1 === "number" || typeof param1 === "string") && param2 === undefined) {
+      this.branches = undefined;
+      this.value = Number(param1);
+    } else if (param1 instanceof Node && param2 instanceof Node) {
+      this.branches = [param1, param2];
+      this.value = undefined;
+    } else {
+      throw new Error(`Error construction parameters: ${param1}, ${param2}`);
+    }
+  }
+
+  get left(): Node {
+    if (this.isLeaf()) throw new Error("Cannot called left getter on a leaf node.");
+    return this.branches![0];
+  }
+
+  get right(): Node {
+    if (this.isLeaf()) throw new Error("Cannot called right getter on a leaf node.");
+    return this.branches![1];
+  }
+
+  isLeaf(): boolean {
+    return this.value !== undefined;
+  }
+
+  magnitude(): number {
+    if (this.isLeaf()) return this.value as number;
+    return this.left.magnitude() * 3 + this.right.magnitude() * 2;
+  }
+
+  toString(): string {
+    return this.isLeaf()
+      ? String(this.value)
+      : `[${this.left.toString()},${this.right.toString()}]`;
+  }
+
+  reduce(): Node {
+    return this;
+  }
+
+  sum(input: string | Node): Node {
+    if (this.isLeaf()) throw new Error("Cannot perform sum() operation on a leaf node.");
+
+    const inputNode = typeof input === "string" ? Snailfish.parse(input) : input;
+    return new Node(this, inputNode).reduce();
+  }
+
+  // static reduce(tree: Node): Node {
+  //   let updated = true;
+
+  //   while (updated) {
+  //     updated = this.recExplode(tree)[0] !== Op.Null;
+  //     // If there is no explode, we start to handle split
+  //     // ref: https://www.reddit.com/r/adventofcode/comments/rjqekd/2021_day_18_not_able_to_find_where_my_logic_goes/
+  //     if (!updated) updated = this.recSplit(tree);
+
+  //     console.dir(tree, {depth: null});
+
+  //   }
+  //   return tree;
+  // }
+
+
+}
+
+// type Payload = [number, number] | undefined;
+
+// const enum Op {
+//   Split,
+//   Explode,
+//   ExplodeImmidiate,
+//   Null,
+// }
+
 class Snailfish {
-  static parseToTree(input: string): [Node, number] {
+  static recParse(input: string): [Node, number] {
     let cursor = 1;
     let bComma = false;
 
-    let left: Node | number | undefined;
-    let right: Node | number | undefined;
-    let endCursor: number | undefined;
+    let left: Node | undefined;
+    let right: Node | undefined;
+    let endCursor: number = cursor;
 
     while (cursor < input.length) {
       if (input.charAt(cursor) === "[") {
         if (bComma) {
-          [right, endCursor] = this.parseToTree(input.slice(cursor));
+          [right, endCursor] = this.recParse(input.slice(cursor));
         } else {
-          [left, endCursor] = this.parseToTree(input.slice(cursor));
+          [left, endCursor] = this.recParse(input.slice(cursor));
         }
-
         cursor += endCursor;
         continue;
-        //
       } else if (input.charAt(cursor) === ",") {
         bComma = true;
         cursor += 1;
         continue;
-        //
       } else if (input.charAt(cursor) === "]") {
-        return [{ left: left!, right: right! }, cursor + 1];
-        //
+        return [new Node(left!, right!), cursor + 1];
       } else {
         // handling numbers
         endCursor = cursor;
@@ -53,12 +121,13 @@ class Snailfish {
           endCursor++;
         }
 
-        if (bComma) {
-          right = Number(input.slice(cursor, endCursor));
-        } else {
-          left = Number(input.slice(cursor, endCursor));
-        }
+        const leafNode = new Node(input.slice(cursor, endCursor));
 
+        if (bComma) {
+          right = leafNode;
+        } else {
+          left = leafNode;
+        }
         cursor = endCursor;
       }
     }
@@ -66,68 +135,92 @@ class Snailfish {
     throw new Error("Unexpected end of input");
   }
 
-  // The return value is a tuple: [boolean, ExplodePayload]:
+  static parse(input: string): Node {
+    return this.recParse(input)[0];
+  }
+
+  // The return value is a tuple: [boolean, Payload]:
   // - `boolean`: indicates there there is a node reduection in this level or below.
-  // - `ExplodePayload`: indicates the unprocessed exploded values, both on left and
+  // - `Payload`: indicates the unprocessed exploded values, both on left and
   //    right side. `Split` operation has no payload, i.e. `undefined`.
-  static recReduce(node: Node | number, depth = 0): [boolean, ExplodePayload] {
-    if (typeof node === "number" && node >= SPLIT_THRESHOLD) {
-      // Handling split
-      const left = Math.floor(node / 2);
-      node = { left, right: node - left };
-      return [true, undefined];
-    } else if (typeof node === "object") {
-      if (depth >= EXPLODE_DEPTH) {
-        const payload: ExplodePayload = [node.left as number, node.right as number];
-        node = 0;
-        return [true, payload];
-      }
+  // static recExplode(node: Node, depth = 0): [Op, Payload] {
+  //   if (depth >= EXPLODE_DEPTH) {
+  //     return [Op.ExplodeImmidiate, [node.left as number, node.right as number]];
+  //   }
 
-      // dealing on the left children
-      let [updated, payload] = this.recReduce(node.left, depth + 1);
-      if (updated) {
-        if (!payload) return [updated, undefined];
+  //   // dealing on the left branch
+  //   let [op, payload] = this.recExplode(node.left as Node, depth + 1);
 
-        if (payload[1] > 0) this.pushToChildren(payload[1], node.right, Direction.Left);
-        return [updated, [payload[0], 0]];
-      }
+  //   if (op === Op.ExplodeImmidiate) node.left = 0;
 
-      // dealing on the right children
-      [updated, payload] = this.recReduce(node.right, depth + 1);
-      if (updated) {
-        if (!payload) return [updated, undefined];
+  //   if (op === Op.ExplodeImmidiate || op === Op.Explode) {
+  //     if (payload![1] > 0) {
+  //       // handle the right value of the payload
+  //       typeof node.right === "number"
+  //         ? (node.right += payload![1])
+  //         : this.pushToChildren(payload![1], node.right, Direction.Left);
+  //     }
+  //     return [Op.Explode, [payload![0], 0]];
+  //   }
 
-        if (payload[0] > 0) this.pushToChildren(payload[0], node.left, Direction.Right);
-        return [updated, [0, payload[1]]];
-      }
-    }
+  //   // dealing on the right branch
+  //   [op, payload] = this.recExplode(node.right as Node, depth + 1);
 
-    return [false, undefined];
-  }
+  //   if (op === Op.ExplodeImmidiate) node.right = 0;
 
-  static pushToChildren(value: number, node: Node | number, dir: Direction): void {
-    if (typeof node === "number") {
-      node += value;
-    } else {
-      this.pushToChildren(value, dir === Direction.Left ? node.left : node.right, dir);
-    }
-  }
+  //   if (op === Op.ExplodeImmidiate || op === Op.Explode) {
+  //     if (payload![0] > 0) {
+  //       // handle the left value of the payload
+  //       typeof node.left === "number"
+  //         ? (node.left += payload![0])
+  //         : this.pushToChildren(payload![0], node.left, Direction.Right);
+  //     }
+  //     return [Op.Explode, [0, payload![1]]];
+  //   }
 
-  static reduceTree(tree: Node): Node {
-    let updated = true;
+  //   return [Op.Null, undefined];
+  // }
 
-    while (updated) {
-      updated = false;
-      [updated] = this.recReduce(tree);
-    }
+  // static pushToChildren(value: number, node: Node, dir: Direction): void {
+  //   if (dir === Direction.Left && typeof node.left === "number") {
+  //     node.left += value;
+  //     return;
+  //   }
 
-    return tree;
-  }
+  //   if (dir === Direction.Right && typeof node.right === "number") {
+  //     node.right += value;
+  //     return;
+  //   }
 
-  static magnitude(node: Node | number): number {
-    if (typeof node === "number") return node;
-    return this.magnitude(node.left) * 3 + this.magnitude(node.right) * 2;
-  }
+  //   return this.pushToChildren(
+  //     value,
+  //     dir === Direction.Left ? node.left as Node : node.right as Node,
+  //     dir,
+  //   );
+  // }
+
+  // static recSplit(node: Node): boolean {
+  //   let updated = false;
+
+  //   if (typeof node.left === "number" && node.left >= SPLIT_THRESHOLD) {
+  //     const left = Math.floor(node.left / 2);
+  //     node.left = { left, right: node.left - left };
+  //     return true;
+  //   }
+
+  //   if (typeof node.right === "number" && node.right >= SPLIT_THRESHOLD) {
+  //     const left = Math.floor(node.right / 2);
+  //     node.right = { left, right: node.right - left };
+  //     return true;
+  //   }
+
+  //   if (typeof node.left === "object" && typeof node.right === "object") {
+  //     updated = this.recSplit(node.left);
+  //     if (!updated) updated = this.recSplit(node.right);
+  //   }
+
+  //   return updated;
+  // }
 }
 
 export { Snailfish as default, Node };
