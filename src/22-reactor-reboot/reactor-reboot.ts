ff21316c2e31c7a06e90f22ed36ebd4743e53f9e
Jimmy Chu <jimmychu0807@gmail.com>
2023-01-24 14:08:51 +0800

    another go

--- src/22-reactor-reboot/reactor-reboot.ts
+++ src/22-reactor-reboot/reactor-reboot.ts
@@ -5,80 +5,45 @@ interface Range {
   max: number;
 }
 
-type Instruction = [boolean, Range, Range, Range];
-
-interface GridConfig {
-  x: OffsetAndLen;
-  y: OffsetAndLen;
-  z: OffsetAndLen;
+interface Instruction {
+  on: boolean;
+  x: Range;
+  y: Range;
+  z: Range;
 }
 
-interface OffsetAndLen {
-  offset: number;
-  len: number;
-}
+type Cuboid = Instruction;
 
 class ReactorReboot {
-  static getLineInfo(line: string): Instruction {
+  static parseInstruction(line: string): Instruction {
     const [onOffStr, afterSpace] = line.split(" ");
     const [xStr, yStr, zStr] = afterSpace!.split(",");
     const [xMin, xMax] = xStr!.slice(2).split("..");
     const [yMin, yMax] = yStr!.slice(2).split("..");
     const [zMin, zMax] = zStr!.slice(2).split("..");
 
-    return [
-      onOffStr === "on",
-      { min: Number(xMin), max: Number(xMax) },
-      { min: Number(yMin), max: Number(yMax) },
-      { min: Number(zMin), max: Number(zMax) },
-    ];
+    return {
+      on: onOffStr === "on",
+      x: { min: Number(xMin), max: Number(xMax) },
+      y: { min: Number(yMin), max: Number(yMax) },
+      z: { min: Number(zMin), max: Number(zMax) },
+    };
   }
 
-  static processOneLine(
+  static processRebootInit(
     domains: boolean[][][],
-    lineInfo: [boolean, Range, Range, Range],
+    ins: Instruction,
     offset: [number, number, number],
   ) {
-    console.log("processOneLine:", lineInfo, offset);
-
     const [xOffset, yOffset, zOffset] = offset;
-    const [xFrom, xTo] = [lineInfo[1].min - xOffset, lineInfo[1].max - xOffset];
-    const [yFrom, yTo] = [lineInfo[2].min - yOffset, lineInfo[2].max - yOffset];
-    const [zFrom, zTo] = [lineInfo[3].min - zOffset, lineInfo[3].max - zOffset];
+    const [xFrom, xTo] = [ins.x.min - xOffset, ins.x.max - xOffset];
+    const [yFrom, yTo] = [ins.y.min - yOffset, ins.y.max - yOffset];
+    const [zFrom, zTo] = [ins.z.min - zOffset, ins.z.max - zOffset];
 
     for (let x = xFrom; x <= xTo; x++) {
       for (let y = yFrom; y <= yTo; y++) {
         for (let z = zFrom; z <= zTo; z++) {
-          domains[x]![y]![z] = lineInfo[0];
-        }
-      }
-    }
-  }
-
-  static toDomainIdxAndBitPos(x: number, y: number, z: number, gc: GridConfig): [number, number] {
-    const pos =
-      (x - gc.x.offset) * gc.y.len * gc.z.len + (y - gc.y.offset) * gc.z.len + (z - gc.z.offset);
-    return [Math.floor(pos / 32), pos % 32];
-  }
-
-  static setDomainBit(domains: number[], di: number, dbp: number, onOff: boolean) {
-    domains[di] = onOff ? domains[di]! | (1 << dbp) : domains[di]! & ~(1 << dbp);
-  }
-
-  static processOneLine2(
-    domains: number[],
-    lineInfo: [boolean, Range, Range, Range],
-    gc: GridConfig,
-  ) {
-    const [xFrom, xTo] = [lineInfo[1].min, lineInfo[1].max];
-    const [yFrom, yTo] = [lineInfo[2].min, lineInfo[2].max];
-    const [zFrom, zTo] = [lineInfo[3].min, lineInfo[3].max];
-
-    for (let x = xFrom; x <= xTo; x++) {
-      for (let y = yFrom; y <= yTo; y++) {
-        for (let z = zFrom; z <= zTo; z++) {
-          const [di, dbp] = this.toDomainIdxAndBitPos(x, y, z, gc);
-          this.setDomainBit(domains, di, dbp, lineInfo[0]);
+          domains[x]![y]![z] = ins.on;
         }
       }
     }
@@ -93,87 +58,112 @@ class ReactorReboot {
     );
   }
 
-  static countOn2(domains: number[]): number {
-    let cnt = 0;
-    for (let i = 0; i < domains.length; i++) {
-      const val = domains[i]!;
-      for (let j = 0; j < 32; j++) {
-        if ((val & (1 << j)) !== 0) cnt++;
-      }
-    }
-    return cnt;
-  }
-
   static initDomains(xLen: number, yLen?: number, zLen?: number): boolean[][][] {
-    console.log(`initDomains: ${xLen}, ${yLen}, ${zLen}`);
-
     return new Array(xLen)
       .fill(0)
       .map(() => new Array(yLen ?? xLen).fill(0).map(() => new Array(zLen ?? xLen).fill(false)));
   }
 
-  static getOffsetAndLen(ranges: Range[]): [number, number] {
-    const minMin = Math.min(...ranges.map((r) => r.min));
-    const maxMax = Math.max(...ranges.map((r) => r.max));
-    return [minMin, maxMax - minMin + 1];
-  }
-
-  static processRebootSteps(input: string[]): number {
+  static initRebootSteps(input: string[]): number {
     // init the cube
     const sideLen = BOUNDARY * 2 + 1;
     const domains: boolean[][][] = this.initDomains(sideLen);
 
     input.forEach((ln) => {
-      const res = this.getLineInfo(ln);
+      const ins = this.parseInstruction(ln);
       if (
-        res[1]!.min > BOUNDARY ||
-        res[1]!.max < BOUNDARY * -1 ||
-        res[2]!.min > BOUNDARY ||
-        res[2]!.max < BOUNDARY * -1 ||
-        res[3]!.min > BOUNDARY ||
-        res[3]!.max < BOUNDARY * -1
+        ins.x.min > BOUNDARY ||
+        ins.x.max < BOUNDARY * -1 ||
+        ins.y.min > BOUNDARY ||
+        ins.y.max < BOUNDARY * -1 ||
+        ins.z.min > BOUNDARY ||
+        ins.z.max < BOUNDARY * -1
       )
         return;
 
-      this.processOneLine(domains, res, [BOUNDARY * -1, BOUNDARY * -1, BOUNDARY * -1]);
+      this.processRebootInit(domains, ins, [BOUNDARY * -1, BOUNDARY * -1, BOUNDARY * -1]);
     });
 
     return this.countOn(domains);
   }
 
-  static initDomains2(xLen: number, yLen?: number, zLen?: number): number[] {
-    const product = xLen * (yLen ?? xLen) * (zLen ?? xLen);
-    return new Array(product % 32 === 0 ? product / 32 : Math.floor(product / 32) + 1).fill(0);
-  }
+  static processRebootSteps(input: string[]): number {
+    const intersectionSets: Cuboid[][] = []
 
-  static getGridConfigFromIns(ins: Instruction[]): GridConfig {
-    const [xOffset, xLen] = this.getOffsetAndLen(ins.map((i) => i[1]));
-    const [yOffset, yLen] = this.getOffsetAndLen(ins.map((i) => i[2]));
-    const [zOffset, zLen] = this.getOffsetAndLen(ins.map((i) => i[3]));
-
-    return {
-      x: { offset: xOffset, len: xLen },
-      y: { offset: yOffset, len: yLen },
-      z: { offset: zOffset, len: zLen },
-    };
-  }
-
-  static processFullReboot(input: string[]): number {
-    // Find the offset and length for x, y, z axis.
-    const instructions = input.map(this.getLineInfo);
-    const gc: GridConfig = this.getGridConfigFromIns(instructions);
-
-    console.log("config", gc);
-
-    const domains: number[] = this.initDomains2(gc.x.len, gc.y.len, gc.z.len);
-
-    console.log(`domain len: ${domains.length}`);
-
-    instructions.forEach((ins) => {
-      this.processOneLine2(domains, ins, gc);
+    input.forEach(ln => {
+      const ins = this.parseInstruction(ln);
+      for(let i = 0; i < intersectionSets.length; i ++) {
+        if (this.hasIntersection(intersectionSets[i]!, ins)) {
+          intersectionSets[i]!.push(ins);
+          return;
+        }
+      }
+      // we don't need to push an OFF cuboid in a new set.
+      if (ins.on) intersectionSets.push([ins]);
     });
 
-    return this.countOn2(domains);
+    return intersectionSets.reduce((memo, set) => memo + this.recGetOnVolume(set), 0);
+  }
+
+  static hasIntersection(set: Cuboid[], cuboid: Cuboid): boolean {
+    for (let i = 0; i < set.length; i++) {
+      if (set[i]!.x.max >= cuboid.x.min && set[i]!.x.min <= cuboid.x.max
+        && set[i]!.y.max >= cuboid.y.min && set[i]!.y.min <= cuboid.y.max
+        && set[i]!.z.max >= cuboid.z.min && set[i]!.z.min <= cuboid.z.max) return true;
+    }
+    return false;
+  }
+
+  static recGetOnVolume(set: Cuboid[], pick: number = 1): number {
+    return pick < set.length
+      ? this.sumOfIntersectionVol(set, pick) - this.recGetOnVolume(set, pick + 1)
+      : this.sumOfIntersectionVol(set, pick);
+  }
+
+  static sumOfIntersectionVol(set: Cuboid[], pick: number): number {
+    const indices = new Array(set.length).fill(0).map((_, idx) => idx);
+    return this.setCombination(indices, pick).reduce((memo, com) => {
+      const subset = com.map(idx => set[idx]!);
+      return pick === 1 && !subset[0]!.on ? memo : memo + this.intersectionVol(subset);
+    }, 0);
+  }
+
+  static setCombination(array: number[], pick: number): number[][] {
+    if (pick === array.length) return [[...array]];
+    if (pick === 0) return new Array(array.length).fill(0).map(() => []);
+
+    let result: number[][] = [];
+    for(let idx = 0; idx < array.length; idx++) {
+      const skipIdxArr = array.filter((_, i) => i !== idx);
+      let res = this.setCombination(skipIdxArr, pick - 1).map(set => set.concat([array[idx]!]));
+      result = result.concat(res);
+    }
+    return result;
+  }
+
+  static intersectionVol(set: Cuboid[]): number {
+    const x: Range = { min: set[0]!.x.min, max: set[0]!.x.max };
+    const y: Range = { min: set[0]!.y.min, max: set[0]!.y.max };
+    const z: Range = { min: set[0]!.z.min, max: set[0]!.z.max };
+
+    for (let i = 1; i < set.length; i++) {
+      if (set[i]!.x.max >= x.min && set[i]!.x.min <= x.max
+        && set[i]!.y.max >= y.min && set[i]!.y.min <= y.max
+        && set[i]!.z.max >= z.min && set[i]!.z.min <= z.max) {
+
+        x.min = Math.max(x.min, set[i]!.x.min);
+        x.max = Math.min(x.max, set[i]!.x.max);
+        y.min = Math.max(y.min, set[i]!.y.min);
+        y.max = Math.min(y.max, set[i]!.y.max);
+        z.min = Math.max(z.min, set[i]!.z.min);
+        z.max = Math.min(z.max, set[i]!.z.max);
+      } else {
+        // There is no intersection point among this cuboid set.
+        return 0;
+      }
+    }
+
+    return (x.max - x.min + 1) * (y.max - y.min + 1) * (z.max - z.min + 1);
   }
 }
 
